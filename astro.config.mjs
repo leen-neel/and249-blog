b4bfd1542cf969ee978e915ee22a33e5c341d261
leen-neel <and24903@gmail.com>
2022-12-25 00:53:41 +0530

    added basic styles for headers and paragraphs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     integrations: [],
+    markdown: {
+        shikiConfig: {
+            theme: 'material-ocean',
+        },
+    },
 });
