460bdd4bc1496cd34d2c986b4676a7c725ca51e7
leen-neel <and24903@gmail.com>
2022-12-25 02:24:00 +0530

    structured blog page

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const SITE_TITLE = 'Neel Writes 🖊';
+export const DESCRIPTION =
+    "Hey there I am Neel! I'm a full-stack web developer and this is my blog where I share my learnings and random things from my life.";
