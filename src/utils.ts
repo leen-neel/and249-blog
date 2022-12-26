765813cd0905d4d0daedcfb7e3c37f9b787cd4dc
leen-neel <and24903@gmail.com>
2022-12-27 00:29:50 +0530

    fixesa

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,4 @@
 export function getImageURL(image?: string): string {
-    console.log(image);
-
     return image
         ? `https://images.and249.codes/${image}`
         : 'https://images.and249.codes/placeholder-social.jpg';
