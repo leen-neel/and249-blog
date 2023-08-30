36b6d79c55f0387070c2fa088f30369a728f03aa
leen-neel <and24903@gmail.com>
2023-08-30 21:37:48 +0530

    updated to astro 3; added viewtransitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 // import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://blog.and249.codes/',
-    integrations: [sitemap()],
-    markdown: {
-        shikiConfig: {
-            theme: 'material-ocean',
-        },
-        drafts: true,
+  site: "https://blog.and249.codes/",
+  integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "material-theme-ocean",
     },
+    drafts: true,
+  },
 });
--- src/content/blog/mystery-is-a-magical-tour.md
+++ src/content/blog/mystery-is-a-magical-tour.md
@@ -1,5 +1,4 @@
 ---
-layout: "../../layouts/BlogLayout.astro"
 title: "Mystery is a Magical Tour: An Interview with Terry Draper"
 description: "After a lot of procrastination, I have finally gotten around in using Astro, and I do hate my past self for procrastinating this much."
 pubDate: "06.15.2023"
@@ -11,3 +10,5 @@ tags:
   - programming
   - astro
 ---
+
+I've had the honor to interview Terry Drapper from Klaatu, one of my all time favorite bands from the 1970s.
